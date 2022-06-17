eb97fcd84a86e3671892822945de570473bbe6a4
parsa__sbr <parsa.sabrian22@gmail.com>
2022-06-17 21:56:53 +0430

    server commit

--- src/main/java/ClientMain.java
+++ src/main/java/ClientMain.java
@@ -1,5 +1,4 @@
 public class ClientMain {
     public static void main(String[] args) {
-
     }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
+import java.util.Scanner;
 import java.util.SortedMap;
 import java.util.Vector;
 
@@ -19,10 +21,19 @@ public class Server {
             ServerSocket serverSocket = new ServerSocket(8080);
             Socket hostSocket = serverSocket.accept();
 
-            ClientHandler host = new ClientHandler(hostSocket);
-            clientHandlers.add(host);
+            Scanner scanner = new Scanner(hostSocket.getInputStream());
+            PrintWriter printWriter = new PrintWriter(hostSocket.getOutputStream());
 
-            int n = host.getPrintWriter().println();
+            String line = "";
+
+            while (line.equals("")) {
+                try {
+                    line = scanner.nextLine();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println(line);
 
         } catch (IOException e) {
             e.printStackTrace();
